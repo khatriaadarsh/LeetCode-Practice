62347185e9e242540e6dbe77a15707da0cfecd8a
khatriaadarsh <aadarshkumark2003@gmail.com>
2025-02-03 16:20:19 +0500

    Two sum

--- script.js
+++ script.js
@@ -225,6 +225,21 @@
 
 // console.log(arr);
 
+// ----------> Two Sum
+
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return [];
+// }
+
+// console.log(twoSum([3, 2, 3], 6)); // [0, 2]
+
 // -------------> Merge Sorted Array
 
 // function mergeSortedArray(arr1, m, arr2, n) {
